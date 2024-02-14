c5d33e032dbd1d1cd60275a3628d72985ff4e326
Dibyendu <dibyendu01kar@gmail.com>
2024-02-14 07:21:34 +0530

    Implementation of @ConfigurationProperties for reading configurations through application.yml

--- account-service/src/main/java/com/dibyendu/accountservice/controller/AccountsController.java
+++ account-service/src/main/java/com/dibyendu/accountservice/controller/AccountsController.java
@@ -1,5 +1,6 @@
 package com.dibyendu.accountservice.controller;
 
+import com.dibyendu.accountservice.Dto.AccountsContactInfoDto;
 import com.dibyendu.accountservice.Dto.CustomerDto;
 import com.dibyendu.accountservice.Dto.response.ErrorResponseDto;
 import com.dibyendu.accountservice.Dto.response.ResponseDto;
@@ -15,6 +16,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -31,8 +34,17 @@ import org.springframework.web.bind.annotation.*;
 @Validated
 public class AccountsController {
 
-    private AccountService accountService;
 
+    private final AccountService accountService;
+    private Environment env;
+    private final AccountsContactInfoDto accountsContactInfoDto;
+
+    @Autowired
+    public AccountsController(AccountService accountService, AccountsContactInfoDto accountsContactInfoDto, Environment env) {
+        this.accountService = accountService;
+        this.accountsContactInfoDto = accountsContactInfoDto;
+        this.env = env;
+    }
 
     @Operation(
             summary = "CREATE account rest api",
@@ -43,7 +55,7 @@ public class AccountsController {
             description = "HTTP status CREATED"
     )
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> createAccount(@Valid  @RequestBody CustomerDto customersDto) {
+    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customersDto) {
 
         accountService.createAccount(customersDto);
 
@@ -61,8 +73,8 @@ public class AccountsController {
             description = "HTTP status OK"
     )
     @GetMapping("/fetch")
-    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam @Pattern(regexp = "(^$|[0-9]{10})",message = "Please enter a valid mobile number")
-                                                               String mobileNumber) throws ResourceNotFoundException {
+    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Please enter a valid mobile number")
+                                                           String mobileNumber) throws ResourceNotFoundException {
         CustomerDto customerDto = accountService.fetchAccount(mobileNumber);
 
         return ResponseEntity.status(HttpStatus.OK).body(customerDto);
@@ -90,10 +102,10 @@ public class AccountsController {
                             )
                     )
             )
-        }
+    }
     )
     @PutMapping("/update")
-    public ResponseEntity<ResponseDto> updateAccountDetails(@Valid  @RequestBody CustomerDto customerDto) throws ResourceNotFoundException {
+    public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) throws ResourceNotFoundException {
         boolean isUpdated = accountService.updateAccount(customerDto);
 
         if (isUpdated) {
@@ -128,15 +140,26 @@ public class AccountsController {
     }
     )
     @DeleteMapping("/delete")
-    public ResponseEntity<ResponseDto> deleteAccount(@RequestParam @Pattern(regexp = "(^$|[0-9]{10})",message = "mobile number must be 10 digits")
-                                                         String mobileNumber) throws ResourceNotFoundException {
-        boolean isDeleted=accountService.deleteAccount(mobileNumber);
+    public ResponseEntity<ResponseDto> deleteAccount(@RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "mobile number must be 10 digits")
+                                                     String mobileNumber) throws ResourceNotFoundException {
+        boolean isDeleted = accountService.deleteAccount(mobileNumber);
 
-        if (isDeleted){
-            return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(AccountsConstants.STATUS_200,AccountsConstants.MESSAGE_200));
+        if (isDeleted) {
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+        } else {
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_DELETE));
         }
-        else {
-            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseDto(AccountsConstants.STATUS_417,AccountsConstants.MESSAGE_417_DELETE));
-        }
+    }
+
+    @Operation(hidden = true)
+    @GetMapping("getUser")
+    public ResponseEntity<String> getUser() {
+        String user = env.getProperty("USERNAME");
+        return ResponseEntity.ok().body(user);
+    }
+
+    @GetMapping("/getContactInfo")
+    public ResponseEntity<AccountsContactInfoDto> getContactInfo() {
+        return ResponseEntity.ok().body(accountsContactInfoDto);
     }
 }
