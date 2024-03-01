4b5036c9ce67204efd0db65db9e85b502dc125f9
Dibyendu <dibyendu01kar@gmail.com>
2024-03-01 09:19:09 +0530

    updating configurations at run time using spring boot actuator 'refresh' end point.

--- account-service/src/main/java/com/dibyendu/accountservice/Dto/AccountsContactInfoDto.java
+++ account-service/src/main/java/com/dibyendu/accountservice/Dto/AccountsContactInfoDto.java
@@ -1,10 +1,20 @@
 package com.dibyendu.accountservice.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+// to change the data at runtime, we're changing the record type to class.(As record made all the value final)
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message, Map<String,String> contactDetails, List<String> customerCare) {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountsContactInfoDto{
+    String message;
+    Map<String,String> contactDetails;
+    List<String> customerCare;
 }
--- cards-service/src/main/java/com/dibyendu/cardsservice/dto/CardsContactInfoDto.java
+++ cards-service/src/main/java/com/dibyendu/cardsservice/dto/CardsContactInfoDto.java
@@ -1,10 +1,20 @@
 package com.dibyendu.cardsservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+// to change the data at runtime, we're changing the record type to class.(As record made all the value final)
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDto(String message, Map<String,String> contactDetails, List<String> customerCare) {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CardsContactInfoDto{
+    String message;
+    Map<String,String> contactDetails;
+    List<String> customerCare;
 }
